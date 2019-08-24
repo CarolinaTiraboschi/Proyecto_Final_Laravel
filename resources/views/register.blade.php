b4458fc32899ded59bff793e6e343080a852636d
CarolinaTiraboschi <carotiraboschi@gmail.com>
2019-08-24 12:40:08 -0300

    Registro muestra errores en javascript

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,31 +12,76 @@
       <h3>EL ENCUENTRO FORTUITO </h3>
     </div>
 
-    <form method="post" enctype="multipart/form-data">
-        <div class="row">
-          <div class="col-10 formulario">
-            <h2 class= "registrar" >REGISTRARME</h2>
+<form action="/register" method="post" enctype="multipart/form-data">
+  <h2 class= "registrar" >REGISTRARME</h2>
+
+
+    <div class="row formulario ">
+      <div class="col-12">
+        <div class="form-group">
+          <input type="text" name="username" class="form-control" placeholder="Nombre de Usuario" value="{{ old('username') }}">
+          @error ('title')
+            <i style="color: red;"> {{ $errors->first('title') }}</i>
+          @enderror
+        </div>
+      </div>
+
+      <div class="col-6">
             <div class="form-group">
-              <input type="text" name="user" class="form-control" placeholder="Nombre de Usuario">
+              <input type="text" name="name" class="form-control" placeholder="Nombre Completo"value="{{ old('name') }}">
 
             </div>
+      </div>
 
-            <div class="form-group">
-              <input type="text" name="name" class="form-control" placeholder="Nombre Completo">
-
-            </div>
+      <div class="col-6">
             <div class="form-group">
               <input type="email" name="email" class="form-control" placeholder="Correo Electrónico">
+              <div class="invalid">
+                <!-- Mensaje de error -->
+              </div>
 
             </div>
+      </div>
+
+      <div class="col-12">
+  					<div class="form-group">
+  						<select name="country" class="form-control">
+  							<option value="">País de nacimiento</option>
+  						</select>
+  						<div class="invalid">
+  							<!-- Mensaje de error -->
+  						</div>
+  					</div>
+  				</div>
+
+      <div class="col-6">
             <div class="form-group">
               <input type="password" name="pass" class="form-control" placeholder="Contraseña">
 
             </div>
+      </div>
+
+      <div class="col-6">
             <div class="form-group">
               <input type="password" name="pass-repeat" class="form-control" placeholder="Repita su contraseña">
 
             </div>
+      </div>
+
+      <div class="col-12">
+					<div class="custom-file">
+						<input type="file" class="custom-file-input" name="avatar">
+    				<label class="custom-file-label">Subir imagen de perfil (Opcional)</label>
+					</div>
+					@error ('avatar')
+						<i style="color: red;"> {{ $errors->first('avatar') }}</i>
+					@enderror
+
+				</div>
+
+
+
+
             <div class="col-10 reg-button">
 
             <button type="submit" class="btn btn-primary">Registrarse</button>
@@ -47,7 +92,11 @@
           </div>
         </div>
       </div>
-    </div>
+
+      <script src="/js/jquery-3.4.1.min.js"></script>
+  	  <script src="/js/bootstrap.min.js"></script>
+    	<script src="/js/getCountries.js"></script>
+      <script src="/js/registerValidate.js"></script>
 
     <style>
     @media (min-width: 600px) {
