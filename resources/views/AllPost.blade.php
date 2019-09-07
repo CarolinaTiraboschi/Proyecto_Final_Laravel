30db99a560db1d1d3543a5ecd404954c002e0660
CarolinaTiraboschi <carotiraboschi@gmail.com>
2019-09-07 17:16:53 -0300

    prueba

--- resources/views/AllPost.blade.php
+++ resources/views/AllPost.blade.php
@@ -1,13 +1,43 @@
-@php
-use App\Post;
-use App\User;
 
 @extends('template')
-@yield('pageTitle', 'Todos los post de')
+@section('pageTitle', 'Todos los post de')
 
-@yield('mainContent')
 
-  @foreach ($posts as $post => $paragraph)
-  {{$post ->'title'}}
+@section('mainContent')
+<br>
+<div class="container profile_general">
+<div class="col-12 profile_username">
+  <h3 style="text-align:center;">Todos los escritos de {{ $theUser }}</h3>
+</div>
 
+<div class= "row justify-content-center profile_posts_section">
+  <div class="card posts_container">
+  @foreach ($posts as $post)
+    <div class="card-text">
+      <div class="card-header" style="text-align:center; font-weight:bold;">{{$post->title}}</div> <br>
+      <div class="card-body">
+        <p class="card-text">{{$post->paragraph}}</p> <br>
+        <a href="#" class="btn btn-primary">Leer más</a>
+
+  <div class="row category_section">
+          @foreach ($post->categories as $category)
+            @php $categories[]= $category->name;
+            @endphp
+          @endforeach
+          @foreach ($categories as $category_name)
+              <div class="col-4 profile_interests_pastillas">
+            <div class="interest" style="text-align:center;"><span>{{$category_name}}</span></div>
+          </div>
+            @php
+              $categories=[];
+            @endphp
+
+          @endforeach
+</div>
+      </div>
+  </div>
   @endforeach
+  </div>
+</div>
+</div>
+@endsection
